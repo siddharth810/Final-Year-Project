f389964403c9b5dab0be103db05dfb69ec2168fb
siddharth810 <48874058+siddharth810@users.noreply.github.com>
2022-02-23 23:58:59 +0530

    Add files via upload

---
+++ Token.sol
@@ -0,0 +1,46 @@
+pragma solidity >=0.4.22 <0.9.0;
+contract Token {
+    string public name = "Token"; 
+    string public symbol = "SSM"; 
+    string  public standard = "SSM Token v1.0";
+    uint256 public totalSupply;
+
+    event Transfer( 
+        address indexed _from, 
+        address indexed _to, 
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner, 
+        address indexed _spender, 
+        uint256 _value
+    ); 
+
+
+    mapping(address => uint256) public balanceOf; 
+    mapping(address => mapping(address => uint256)) public allowance;
+
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply; 
+        //allocate the initial supply
+        totalSupply = _initialSupply;
+    }
+
+    //Transfer 
+
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        //Exception
+        require(balanceOf[msg.sender] >= _value); 
+        //Transfer the balance
+        balanceOf[msg.sender] -= _value; 
+        balanceOf[_to] += _value; 
+
+        //Transfer Event    
+        emit Transfer(msg.sender, _to, _value); 
+        
+        //Return a bool
+        return true; 
+    }
